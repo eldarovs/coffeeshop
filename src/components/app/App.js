60b86aff6928cfdb579617da508490753faf4f4d
eldarovs <eldarovscorporate@gmail.com>
2023-04-23 09:07:08 +0400

    test

---
+++ src/components/app/App.js
@@ -0,0 +1,14 @@
+// import logo from './logo.svg';
+
+import Header from '../header/Header'
+import '../app/App';
+
+function App() {
+  return (
+    <div className="App">
+      <Header/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/header/Header.js
@@ -0,0 +1,9 @@
+import './Header.scss'
+
+const Header = () => {
+    return(
+        <div className='red'>123</div>
+    )
+}
+
+export default Header
